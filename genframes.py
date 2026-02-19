9606b62a59a93bbe8bcb92b050dbf077a3d0ff92
ajq1 <ajq1@sfu.ca>
2026-02-19 01:01:59 -0800

    test

--- genframes.py
+++ genframes.py
@@ -1,5 +1,5 @@
 
 decl = "img{0} = http.get('https://github.com/AlexQ3D/RomeoIsATidbyt/blob/main/frames/tvman{0}.jpg?raw=true').body()"
 
-for i in range(1, 49):
+for i in range(1, 7):
     print(decl.format(i))
